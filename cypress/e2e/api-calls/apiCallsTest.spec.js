def1627fb091191100a9fe8c86e77a542cf5d06c
Andrei Amariei <andrei.amariei@arobs.com>
2024-02-20 10:18:24 +0200

    Cypress API calls

--- cypress/e2e/api-calls/apiCallsTest.spec.js
+++ cypress/e2e/api-calls/apiCallsTest.spec.js
@@ -2,7 +2,8 @@
 
 describe('Api Calls Testing Suite ...', () => {
     beforeEach('Open application', () => {
-        cy.intercept('GET', 'https://fakestoreapi.com/products?sort=desc&limit=12', { fixture: 'products.json' })
+        // cy.intercept('GET', 'https://fakestoreapi.com/products?sort=desc&limit=12', { fixture: 'products.json' })
+        // cy.intercept('GET', 'https://fakestoreapi.com/products*', { fixture: 'products.json' })
         cy.openApplication();
     })
 
@@ -12,5 +13,26 @@ describe('Api Calls Testing Suite ...', () => {
             cy
                 .wrap(item)
         })
+    })
+
+    it('Second API call', () => {
+        cy.log('We are in the application');
+        cy.get('mat-grid-list mat-grid-tile').then(tileList => {
+            expect(tileList[0]).to.contain('1')
+            expect(tileList[1]).to.contain('2')
+        })
+
+        cy.fixture('products').then(product => {
+            const productLink = product[0].price;
+            product[0].price = 22;
+            expect(productLink).to.be.not.eq(product[0].price)
+        })
+    })
+
+    it.only('Third API call', () => {
+        cy.log('We are in the application');
+        cy.get('[data-cy="categories"]').click();
+        cy.intercept('GET', 'https://fakestoreapi.com/products/category/women\'s%20clothing?sort=desc&limit=12', { fixture: 'womenProducts.json' });
+        cy.get('[data-cy="category"]').contains('women\'s clothing').click();
     })
 })
